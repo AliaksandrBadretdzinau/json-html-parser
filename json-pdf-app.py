2e4147dfe548f8d97b0a02a1017f7e01f3e493ad
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-15 17:08:49 +0300

    Update lambda #2

--- json-pdf-app.py
+++ json-pdf-app.py
@@ -82,6 +82,8 @@ def main(event, context):
     template = template_build(event['data']['layout_name'])
     html = template(event['data'])
 
+    asyncio.run(pdf_build(html))
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
