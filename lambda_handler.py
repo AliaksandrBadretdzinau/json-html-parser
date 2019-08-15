f7473a4211744d68fbd5a34c1a21167cc5013ca9
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-15 17:20:17 +0300

    Added layer and function

---
+++ lambda_handler.py
@@ -0,0 +1,19 @@
+import json
+
+
+def lambda_handler(event, context):
+    template = template_build(event['data']['layout_name'])
+    html = template(event['data'])
+    asyncio.run(pdf_build(html))
+
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": dumps(html)
+    }
+
+    return response
