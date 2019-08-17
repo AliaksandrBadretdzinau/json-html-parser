18fddc6ece5dfae0587e0628f43d13d26b977591
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-17 19:07:40 +0300

    Updating

--- get_dirs.py
+++ get_dirs.py
@@ -1,7 +1,8 @@
 import shutil as sh
+from os import path
 
 
-packages = '/usr/local/lib/python3.7/site-packages/'
-sourses = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages/'
+packages = '/usr/local/lib/python3.7/site-packages'
+sourses = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages'
 
-sh.copy(f'{packages}appdirs.py', sourses)
+sh.copy(path.join(packages, 'appdirs.py'), sourses)
