d87bb774bcaee407d6614b7a520cafa3de307a8c
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-18 23:55:41 +0300

    Test WeasyPrint pdf lib

--- libs_builder.py
+++ libs_builder.py
@@ -3,8 +3,7 @@ import os
 import sys
 #import stat
 
-browser_name = 'chrome-lunux'
-browser_path = '/root/.local/share/pyppeteer/local-chromium/575458'
+
 src = '/usr/local/lib/python3.7/site-packages'
 dest = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages'
 libs = (
@@ -15,11 +14,9 @@ libs = (
     'pyppeteer',
     'tqdm',
     'urllib3',
-    'websockets'
+    'websockets',
+    'weasyprint'
 )
-#file = os.path.join(dest, 'JsonPdfEngineLayer.py')
-#st = os.stat(file)
-#os.chmod(file, st.st_mode | stat.S_IEXEC)
 
 for lib in libs:
     obj = os.path.join(src, lib)
@@ -27,9 +24,3 @@ for lib in libs:
         sh.copytree(obj, os.path.join(dest, lib))
     else:
         sh.copy(obj, dest)
-
-sh.copy('headless-chromium/headless-chromium', dest)
-sh.rmtree('headless-chromium')
-# sh.copytree(os.path.join(browser_path, browser_name), os.path.join(dest, browser_name))
-#my_dir = os.path.dirname(dest)
-#os.system('%s %s' % ('python ', file))
