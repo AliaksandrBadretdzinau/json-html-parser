17f01b7b3d84a064ad8009151d5eff5d734bcff0
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-20 02:53:20 +0300

    Copy pango libs

--- libs_builder.py
+++ libs_builder.py
@@ -1,31 +1,49 @@
 import shutil as sh
 import os
 import sys
-#import stat
 
 
-src = '/usr/local/lib/python3.7/site-packages'
-dest = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages'
-libs = (
-    'pybars',
-    'pymeta',
-    'weasyprint',
-    'cssselect2',
-    'webencodings',
-    'tinycss2',
-    'html5lib',
-    'cairocffi',
-    'cffi',
-    '_cffi_backend.cpython-37m-x86_64-linux-gnu.so',
-    '.libs_cffi_backend',
-    'pyphen',
-    'pycparser',
-    'pango-1.0'
-)
+def copy_libs(src, dest, libs)
+    for lib in libs:
+        obj = os.path.join(src, lib)
+        if os.path.isdir(obj):
+            sh.copytree(obj, os.path.join(dest, lib))
+        else:
+            sh.copy(obj, dest)
 
-for lib in libs:
-    obj = os.path.join(src, lib)
-    if os.path.isdir(obj):
-        sh.copytree(obj, os.path.join(dest, lib))
-    else:
-        sh.copy(obj, dest)
+
+def libs_standart():
+    src = '/usr/local/lib/python3.7/site-packages'
+    dest = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages'
+    libs = (
+        'pybars',
+        'pymeta',
+        'weasyprint',
+        'cssselect2',
+        'webencodings',
+        'tinycss2',
+        'html5lib',
+        'cairocffi',
+        'cffi',
+        '_cffi_backend.cpython-37m-x86_64-linux-gnu.so',
+        '.libs_cffi_backend',
+        'pyphen',
+        'pycparser',
+    )
+
+    copy_libs(src, dest, libs)
+
+
+def pango():
+    src = '/usr/lib/x86_64-linux-gnu'
+    dest = 'layers/JsonPdfEnginePath/python/lib/python3.7/site-packages'
+    libs = (
+        'libpango-1.0.so.0',
+        'libpango-1.0.so.0.4000.14',
+        'libpangocairo-1.0.so.0',
+        'libpangocairo-1.0.so.0.4000.14',
+        'libpangoft2-1.0.so.0',
+        'libpangoft2-1.0.so.0.4000.14'
+    )
+
+    copy_libs(src, dest, libs)
