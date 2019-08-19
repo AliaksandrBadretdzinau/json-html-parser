3d29636a30d9cbca7e1507f8873f033c6d95a147
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-19 04:23:07 +0300

    Added pyphen

--- libs_builder.py
+++ libs_builder.py
@@ -17,7 +17,8 @@ libs = (
     'cairocffi',
     'cffi',
     '_cffi_backend.cpython-37m-x86_64-linux-gnu.so',
-    '.libs_cffi_backend'
+    '.libs_cffi_backend',
+    'pyphen'
 )
 
 for lib in libs:
