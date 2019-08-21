7ca4cb2bd89bf4b4a78e2bd6a68e0e20de307e28
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-22 01:35:09 +0300

    Try pdf lib

--- lambda_handler.py
+++ lambda_handler.py
@@ -5,7 +5,7 @@ from json import dumps
 def lambda_function(event, context):
     template = json_pdf_engine.template_build(event['data']['layout_name'])
     html = template(event['data'])
-    #json_pdf_engine.pdf_build(html)
+    json_pdf_engine.pdf_build(html)
 
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
