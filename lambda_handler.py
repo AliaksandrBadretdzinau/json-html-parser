0985ef6a43123ae6938498335775e0cb6dc4a680
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-23 09:07:52 +0300

    Code clean #1

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,21 +1,24 @@
 import JsonPdfEngineLayer as json_pdf_engine
+
 import base64
 import json
 
 
 def lambda_function(event, context):
-    body = json.loads(base64.b64decode(event['body']).decode())
+    data = json.loads(
+        base64.b64decode(event['body']).decode()
+    )
 
-    template = json_pdf_engine.template_build(body['data']['layout_name'])
+    template = json_pdf_engine.template_build(data['data']['layout_name'])
     html = template(body['data'])
     json_pdf_engine.pdf_build(html)
 
-    with open('/tmp/save_me.pdf', 'rb') as f:
+    with open('/tmp/output.pdf', 'rb') as f:
         output = f.read()
 
     return {
-        "isBase64Encoded": True,
         "statusCode": 200,
         "headers": { "content-type": "application/pdf"},
-        "body":  base64.b64encode(output).decode("utf-8")
+        "body":  base64.b64encode(output).decode("utf-8"),
+        "isBase64Encoded": True
     }
