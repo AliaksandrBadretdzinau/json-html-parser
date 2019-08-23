3b22ba7bc7bb5372dff0d6773418cd8406a150e9
Aliaksandr <aliaksandr.badretdzinau@gmail.com>
2019-08-23 06:16:50 +0300

    Work is done!

--- lambda_handler.py
+++ lambda_handler.py
@@ -4,31 +4,10 @@ import json
 
 
 def lambda_function(event, context):
-    print('Source event')
-    print(event)
-    data = {
-        "data": {
-            "layout_name": "base",
-            "title": "Social networks!",
-            "text": "This is place where you can be close with your friends.",
-            "links": [
-                "https://vk.com",
-                "https://twitter.com",
-                "https://facebook.com"
-            ]
-        }
-    }
-    body = event['body']
-    print('BODY:', body)
+    body = json.loads(base64.b64decode(event['body']).decode())
 
-    encoded_body = base64.b64decode(body)
-    print('ENCODED_BODY:', encoded_body)
-
-    body_get = json.loads(encoded_body.decode())
-    print('UTF_BODY:', body_get)
-
-    template = json_pdf_engine.template_build(body_get['data']['layout_name'])
-    html = template(body_get['data'])
+    template = json_pdf_engine.template_build(body['data']['layout_name'])
+    html = template(body['data'])
     json_pdf_engine.pdf_build(html)
 
     with open('/tmp/save_me.pdf', 'rb') as f:
